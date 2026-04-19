09a28e748205e93b6560e78a2d7fd2894052020b
Mazen Marei <moazmazen9199@gmail.com>
2026-04-19 18:18:35 +0200

    fix: adding RequestMapping to map api  with /auth

--- auth-service/src/main/java/com/gotrack/auth_service/controller/TestController.java
+++ auth-service/src/main/java/com/gotrack/auth_service/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.gotrack.auth_service.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class TestController {
 
     @GetMapping("/test")
