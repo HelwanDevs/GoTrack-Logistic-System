7b89b390c27307de4a051b8e2326cb93d00dd856
Mazen Marei <moazmazen9199@gmail.com>
2026-05-03 22:00:09 +0300

    style: make the sidebar has fixed size

--- frontend/admin-dashboard/src/components/Sidebar.tsx
+++ frontend/admin-dashboard/src/components/Sidebar.tsx
@@ -82,7 +82,7 @@ export const Sidebar = () => {
   }, [activeNav]);
 
   return (
-    <nav className="sticky right-0 top-0 h-screen w-64 bg-primary-container flex-col z-40 border-l border-white/10 shadow-xl hidden md:flex">
+    <nav className="sticky right-0 top-0 h-screen w-64 min-w-64 bg-primary-container flex-col z-40 border-l border-white/10 shadow-xl hidden md:flex">
       {/* Logo Section */}
       <div className="p-6 flex flex-col items-center border-b  border-white/10 mb-10">
         <div className="flex items-center justify-center mb-4">
