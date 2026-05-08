6f14054ad986e14fcab733f5dd22871cf6c1f757
Mazen Marei <moazmazen9199@gmail.com>
2026-05-08 20:04:35 +0300

    feat: adding and configuer Feign to handel request Interceptor and forward Internal Token to other services

--- user-branch-service/src/main/java/com/gotrack/user_branch_service/UserBranchServiceApplication.java
+++ user-branch-service/src/main/java/com/gotrack/user_branch_service/UserBranchServiceApplication.java
@@ -2,8 +2,10 @@ package com.gotrack.user_branch_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserBranchServiceApplication {
 
 	public static void main(String[] args) {
---
+++ user-branch-service/src/main/java/com/gotrack/user_branch_service/config/FeignConfig.java
@@ -0,0 +1,52 @@
+package com.gotrack.user_branch_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+
+        return requestTemplate -> {
+
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes();
+
+            if (attributes == null) {
+                return;
+            }
+
+            HttpServletRequest request = attributes.getRequest();
+
+            String authorizationHeader = request.getHeader("X-Internal-Token");
+
+            if (authorizationHeader != null && !authorizationHeader.isBlank()) {
+
+                requestTemplate.header(
+                        "X-Internal-Token",
+                        authorizationHeader);
+            }
+
+            String accountId = request.getHeader("X-Account-Id");
+            if (accountId != null) {
+                requestTemplate.header("X-Account-Id", accountId);
+            }
+            String userId = request.getHeader("X-Email");
+            if (userId != null) {
+                requestTemplate.header("X-Email", userId);
+            }
+
+            String role = request.getHeader("X-User-Role");
+            if (role != null) {
+                requestTemplate.header("X-User-Role", role);
+            }
+        };
+    }
+}
